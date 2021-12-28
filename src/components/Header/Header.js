5847861e9e3f3446db203606329abdcac58eba3c
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-12-28 17:07:42 +0530

    Header v1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
-    <div style={{ backgroundColor: "pink" }}>
-      <h1>
-        <img src="android-chrome-512x512.png" alt="map icon" width="35" />
-        TripBuddy
-      </h1>
-    </div>
+    <AppBar position="static">
+      <Toolbar classNmaes={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          TripBuddy <img src="apple-touch-icon.png" alt="icon map" width="30" />
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore places, hotels , restaurants
+          </Typography>
+          <Autocomplete>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search a place..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
