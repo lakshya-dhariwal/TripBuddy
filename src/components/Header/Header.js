4f75fe93a96cec975fdf937a60714b8a9093c675
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-12-28 16:38:00 +0530

    project-setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>
+        <img src="android-chrome-512x512.png" alt="map icon" width="35" />
+        TripBuddy
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
